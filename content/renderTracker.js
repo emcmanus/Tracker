4b37a2301c6d7a1b17efa775f970e899d91e218e
Ed McManus <edmcmanus@champ.local>
2009-10-03 15:04:48 -0700

    more files

--- content/renderTracker.js
+++ content/renderTracker.js
@@ -1,1 +1,8 @@
-alert("render tracker!");
+var rendertracker = {
+  onLoad: function() {
+    // initialization code
+    window.alert("on load!");
+  }
+};
+
+window.addEventListener("load", function(e) { rendertracker.onLoad(e); }, false);
---
+++ defaults/preferences/rendertracker.js
@@ -0,0 +1,2 @@
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.rendertracker@ed.mcmanus.description", "chrome://rendertracker/locale/rendertracker.properties");
